928c2c5a85163c19816d28d30ff05d12355dc644
Roshan KC <roshankcpkr123@gmail.com>
2022-08-09 15:45:33 +0545

    refactored css

--- index.js
+++ index.js
@@ -1,47 +1,16 @@
-import {classArray} from "./classData.js" 
-
-const mainBody = document.querySelector('main')
-const footerBody = document.querySelector('footer')
-const logo = document.querySelector('.logo')
-const menuIcon = document.querySelector('.hamburger')
-let iconState = true
+const hamburger = document.querySelector(".hamburger")
+const crossMark = document.querySelector(".crossmark")
+const menuOverlay = document.querySelector('.mobile_nav')
 
 
-function checkClassArray()
-{
-    classArray.map((el)=>{
-        return classChange(el.selectClass, el.changeClass)
-    })
+function overlayMenu() {
+    menuOverlay .style.display = "flex";
 }
 
-function displayElement(value)
-{
-    mainBody.style.display = value
-    footerBody.style.display = value
-    logo.style.display = value
-}
-function classChange(selectElement, newClass)
-{
-    document.querySelectorAll(selectElement).forEach(
-        (el)=>{
-            return iconState? el.classList.add(newClass) : el.classList.remove(newClass)
-        }
-    )
+function closeOverlay() {
+    menuOverlay .style.display = "none";
 }
 
-menuIcon.addEventListener("click" ,()=>{
 
-    if (iconState){
-        menuIcon.innerHTML = "&#10005;"
-        checkClassArray()
-        displayElement("none")
-        iconState = false
-    }
-    else{
-        menuIcon.innerHTML = "&#8801;"
-        checkClassArray()
-        displayElement("block")
-        iconState = true
-    }
-
-})
+hamburger.addEventListener("click", overlayMenu)
+crossMark.addEventListener("click", closeOverlay)
